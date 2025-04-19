1090d35269d4e4e152dd16afe59c0d36ecff40b7
habib231-4 <chamadiyahabib00@gmail.com>
2025-04-19 13:38:30 +0530

    AppItem.kt

--- AppItem.kt
+++ AppItem.kt
@@ -1,1 +1,9 @@
-// AppItem content
+package com.yourname.ioslauncherandroidtv
+
+import android.graphics.drawable.Drawable
+
+data class AppItem(
+    val label: String,
+    val packageName: String,
+    val icon: Drawable
+)
